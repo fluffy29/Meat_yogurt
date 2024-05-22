775b886e604f0be4f0e8519e298248e6c04104f5
fluffy29 <hmajzoub12@gmail.com>
2024-05-22 16:16:23 +0200

    chore: Update newsletter form action URL

---
+++ newsletter-signup.php
@@ -0,0 +1,25 @@
+<?php
+
+try {
+    $db = new PDO('mysql:host=127.0.0.1;dbname=meat_yogurt', 'root', '');
+} catch (PDOException $e) {
+    // echo '<pre>';
+    // print_r($e->getMessage());
+    // echo '</pre>';
+    echo '<p> whoops! something went wrong </p>';
+    echo '<br>';
+    echo '<a href=""http://localhost:8080/meat-yogurt-website/">Back to homepage </a>';
+    exit;
+}
+
+print_r($db);
+
+$customer = [
+    'name' => isset ($_POST['name']) ? $_POST['name'] : NULL,
+    'email' => isset ($_POST['email']) ? $_POST['email'] : NULL,
+    'created_at' => Date("Y-m-d"),
+];
+
+echo '<pre>';
+print_r($customer);
+echo '</pre>';
