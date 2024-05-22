f21319da9f60a211e318cc6f5a0dfd6b8a549a80
fluffy29 <hmajzoub12@gmail.com>
2024-05-22 23:12:37 +0200

    chore: Update database connection in newsletter signup script using dbeaver

--- newsletter-signup.php
+++ newsletter-signup.php
@@ -1,25 +1,16 @@
 <?php
 
-try {
-    $db = new PDO('mysql:host=127.0.0.1;dbname=meat_yogurt', 'root', '');
-} catch (PDOException $e) {
-    // echo '<pre>';
-    // print_r($e->getMessage());
-    // echo '</pre>';
-    echo '<p> whoops! something went wrong </p>';
-    echo '<br>';
-    echo '<a href="http://localhost/meat-yogurt/">Back to homepage </a>';
-    exit;
+$hostname = "localhost"; // Replace with your actual hostname
+$username = "root";
+$password = "Acsdoha2023!";
+$dbname = "Meat_yogurt";
+
+$conn = mysqli_connect($hostname, $username, $password, $dbname);
+
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
 }
 
-print_r($db);
+// Execute your database queries here
 
-$customer = [
-    'name' => isset ($_POST['name']) ? $_POST['name'] : NULL,
-    'email' => isset ($_POST['email']) ? $_POST['email'] : NULL,
-    'created_at' => Date("Y-m-d"),
-];
-
-echo '<pre>';
-print_r($customer);
-echo '</pre>';
+mysqli_close($conn);
